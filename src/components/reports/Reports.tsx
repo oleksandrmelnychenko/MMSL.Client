799568cb622b7703e3f79d2e5be604d9e7ef6df5
Andriy Kozyrev <kozurevav@gmail.com>
2020-05-21 14:58:45 +0300

    reports test

--- src/components/reports/Reports.tsx
+++ src/components/reports/Reports.tsx
@@ -1,74 +1,60 @@
 import React, { useState } from 'react';
-import {
-  CommandBar,
-  ICommandBarItemProps,
-  values,
-} from 'office-ui-fabric-react';
+import { CommandBar, ICommandBarItemProps } from 'office-ui-fabric-react';
 import {
   commandBarStyles,
   commandBarButtonStyles,
 } from '../../common/fabric-styles/styles';
-import { FormicReference } from '../../interfaces';
 import ReportsForm from './ReportsForm';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 
 const Reports: React.FC = () => {
-  const initValues = { userName: '' };
+  const initValues = { userName: 'TEST' };
 
-  const _items: ICommandBarItemProps[] = [
-    {
-      key: 'Save',
-      text: 'Save',
-      //   disabled: !isDirtyForm,
-      iconProps: { iconName: 'Save' },
-      onClick: () => {
-        // let formik: any = formikReference.formik;
-        // if (formik !== undefined && formik !== null) {
-        //   formik.submitForm();
-        // }
-      },
-      buttonStyles: commandBarButtonStyles,
-    },
-    {
-      key: 'Reset',
-      text: 'Reset',
-      //   disabled: !isDirtyForm,
-      iconProps: { iconName: 'Refresh' },
-      onClick: () => {
-        // formikReference.formik.resetForm();
-      },
-      buttonStyles: commandBarButtonStyles,
-    },
-  ];
+  // const _items: ICommandBarItemProps[] = ;
   return (
-    <div></div>
-    // <Formik
-    //   initialValues={{ name: 'jared' }}
-    //   onSubmit={(values, actions) => {
-    //     setTimeout(() => {
-    //       alert(JSON.stringify(values, null, 2));
-    //       actions.setSubmitting(false);
-    //     }, 1000);
-    //   }}>
-    //   {(props) => {
-    //     console.log(props);
-    //     return (
-    // <ReportsForm props={props} />
-    //   <form onSubmit={props.handleSubmit}>
-    //     <input
-    //       type="text"
-    //       onChange={props.handleChange}
-    //       onBlur={props.handleBlur}
-    //       value={props.values.name}
-    //       name="name"
-    //     />
-    //     {props.errors.name && <div id="feedback">{props.errors.name}</div>}
-    //     <button type="submit">Submit</button>
-    //   </form>
-    // );
-    // }}
-    // </Formik>
+    <Formik
+      initialValues={initValues}
+      onSubmit={(values, actions) => {
+        console.log(values, 'SUbmit');
+      }}>
+      {(props) => {
+        console.log(props);
+        return (
+          <>
+            <div>
+              <CommandBar
+                styles={commandBarStyles}
+                items={[
+                  {
+                    key: 'Save',
+                    text: 'Save',
+                    disabled: !props.dirty,
+                    iconProps: { iconName: 'Save' },
+                    onClick: () => {
+                      props.handleSubmit();
+                    },
+                    buttonStyles: commandBarButtonStyles,
+                  },
+                  {
+                    key: 'Reset',
+                    text: 'Reset',
+                    disabled: !props.dirty,
+                    iconProps: { iconName: 'Refresh' },
+                    onClick: () => {
+                      props.resetForm();
+                    },
+                    buttonStyles: commandBarButtonStyles,
+                  },
+                ]}
+                className="dealers__store__controls"
+              />
+            </div>
+            <ReportsForm formik={props} />
+          </>
+        );
+      }}
+    </Formik>
   );
 };
 
--- src/components/reports/ReportsForm.tsx
+++ src/components/reports/ReportsForm.tsx
@@ -5,56 +5,50 @@ import { Stack, TextField } from 'office-ui-fabric-react';
 import { FormicReference, StoreCustomer } from '../../interfaces';
 import * as fabricStyles from '../../common/fabric-styles/styles';
 
-interface IProps extends FormikProps<any> {
-  props: any;
+interface IProps {
+  formik: any;
 }
 
 export const ReportsForm: React.FC<IProps> = (props) => {
   console.log(props, 'PROPS');
   return (
-    <div>
-      {/* {(props) => {
-        return (
-          <Form className="form">
-            <div className="dealerFormManage">
-              <Stack horizontal tokens={{ childrenGap: 20 }}>
-                <Stack grow={1}>
-                  <Field name="userName">
-                    {() => (
-                      <div className="form__group">
-                        <TextField
-                          value={props.formik.values.userName}
-                          styles={fabricStyles.textFildLabelStyles}
-                          className="form__group__field"
-                          label="User name"
-                          required
-                          onChange={(args: any) => {
-                            let value = args.target.value;
+    <Form className="form">
+      <div className="dealerFormManage">
+        <Stack horizontal tokens={{ childrenGap: 20 }}>
+          <Stack grow={1}>
+            <Field name="userName">
+              {() => (
+                <div className="form__group">
+                  <TextField
+                    value={props.formik.values.userName}
+                    styles={fabricStyles.textFildLabelStyles}
+                    className="form__group__field"
+                    label="User name"
+                    required
+                    onChange={(args: any) => {
+                      let value = args.target.value;
 
-                            props.formik.setFieldValue('userName', value);
-                            props.formik.setFieldTouched('userName');
-                          }}
-                          errorMessage={
-                            props.formik.errors.userName &&
-                            props.formik.touched.userName ? (
-                              <span className="form__group__error">
-                                {props.formik.errors.userName}
-                              </span>
-                            ) : (
-                              ''
-                            )
-                          }
-                        />
-                      </div>
-                    )}
-                  </Field>
-                </Stack>
-              </Stack>
-            </div>
-          </Form>
-        );
-      }} */}
-    </div>
+                      props.formik.setFieldValue('userName', value);
+                      props.formik.setFieldTouched('userName');
+                    }}
+                    errorMessage={
+                      props.formik.errors.userName &&
+                      props.formik.touched.userName ? (
+                        <span className="form__group__error">
+                          {props.formik.errors.userName}
+                        </span>
+                      ) : (
+                        ''
+                      )
+                    }
+                  />
+                </div>
+              )}
+            </Field>
+          </Stack>
+        </Stack>
+      </div>
+    </Form>
   );
 };
 
